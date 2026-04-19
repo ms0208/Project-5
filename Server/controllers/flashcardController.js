b898081614ca671aaaf23470bf7ce67d6374b958
ms0208 <manav.8230680@gmail.com>
2026-04-19 08:09:12 +0530

    third commit

---
+++ Server/controllers/flashcardController.js
@@ -0,0 +1,37 @@
+import flashcard from '../models/Flashcard.js';
+
+export const getFlashcards = async(req,res,next) =>{
+    try{
+
+    }catch(error){
+        next(error);
+    }
+};
+export const  getAllFlashcardSets = async(req,res,next) =>{
+    try{
+
+    }catch(error){
+        next(error);
+    }
+};
+export const reviewFlashcard = async(req,res,next)=>{
+    try{
+
+    }catch(error){
+        next(error);
+    }
+};
+export const taggleStarFlashcard = async(req,res,next)=>{
+    try{
+
+    }catch(error){
+        next(error);
+    }
+};
+export const deletedFlashcardlist = async(req,res,next)=>{
+    try{
+
+    }catch(error){
+        next(error);
+    }
+};
